7c0608c0a86f88f062d68e6fb92ffe393d0a4bfd
Myoujin <62991167+Myoujin@users.noreply.github.com>
2025-06-23 14:34:09 +0200

    fix build by aliasing beautiful-skill-tree

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'beautiful-skill-tree': 'beautiful-skill-tree/dist/beautiful-skill-tree.esm.js'
+    }
+  },
   server: {
     port: 3000
   }
